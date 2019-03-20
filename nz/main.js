06dbd35576c87e0c14202887b7464bfe6f3f294b
Honka <csaw2326@uibk.ac.at>
2019-03-20 18:05:03 +0100

    Leaflet einbinden und const fuer Daten

---
+++ nz/main.js
@@ -0,0 +1,12 @@
+// Skript für Neuseelandreise;
+
+const div = document.getElementById("map");
+const lat = div.getAttribute("data-lat");
+const lng = div.getAttribute("data-lng");
+const title = div.getAttribute("data-title");
+
+// console.log(lat,lng,title);
+
+// alert(lat);
+// alert(lng);
+// alert(title);
