a5f77f37bf22b7e74cc2fc8c5816f0f63a66b44b
susannehonka <susanne.honka@gmx.de>
2019-03-21 18:19:12 +0100

    workload 3 5/10 Mathematical Assignment Operators

--- workload/3/main.js
+++ workload/3/main.js
@@ -1,5 +1,18 @@
-const entree = 'Enchiladas';
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
 
-console.log(entree);
+// Use the mathematical assignments in the space below:
 
-const testing;
+
+
+
+
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp);
+console.log('The value of powerLevel:', powerLevel);
+console.log('The value of multiplyMe:', multiplyMe);
+console.log('The value of quarterMe:', quarterMe);
