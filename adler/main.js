752531743e81e368ed5c81a201d806a3c003b78e
Honka <csaw2326@uibk.ac.at>
2019-03-27 16:49:40 +0100

    bild geänder, pin punkte gesetzt

--- adler/main.js
+++ adler/main.js
@@ -1,9 +1,12 @@
-// Skript für Neuseelandreise;
+// Skript für Adlerweg;
 
 const div = document.getElementById("map");
-const breite = div.getAttribute("data-lat");
-const laenge = div.getAttribute("data-lng");
-const titel = div.getAttribute("data-title");
+const breite1 = div.getAttribute("data-lat1");
+const laenge1 = div.getAttribute("data-lng1");
+const titel1 = div.getAttribute("data-title1");
+const breite2 = div.getAttribute("data-lat2");
+const laenge2 = div.getAttribute("data-lng2");
+const titel2 = div.getAttribute("data-title2");
 
 // console.log(breite,laenge,titel);
 
@@ -27,10 +30,16 @@ karte.setView(
 // Openstreetmap einbauen
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
-// Positionsmarker hinzufügen
-let pin = L.marker(
-    [breite,laenge]
+// Positionsmarker 1 hinzufügen
+let pin1 = L.marker(
+    [breite1,laenge1]
 ).addTo(karte);
 
-// Popup zum pin hängen
-pin.bindPopup(titel).openPopup();
+// Positionsmarker 2 hinzufügen
+let pin2 = L.marker(
+    [breite2,laenge2]
+).addTo(karte);
+
+// Popup zum pin 1 und 2 hängen
+pin1.bindPopup(titel1).openPopup();
+pin2.bindPopup(titel2).openPopup();
