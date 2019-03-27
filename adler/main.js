04b3231ca85b8337c1f59f9e79982dc9b4b14093
Honka <csaw2326@uibk.ac.at>
2019-03-27 16:05:00 +0100

    main.js eingefügt

---
+++ adler/main.js
@@ -0,0 +1,36 @@
+// Skript für Neuseelandreise;
+
+const div = document.getElementById("map");
+const breite = div.getAttribute("data-lat");
+const laenge = div.getAttribute("data-lng");
+const titel = div.getAttribute("data-title");
+
+// console.log(breite,laenge,titel);
+
+// alert(breite);
+// alert(laenge);
+// alert(titel);
+
+
+
+// Karte initialisieren
+let karte = L.map("map");
+//console.log(karte);
+
+
+// auf Ausschnitt zoomen
+karte.setView(
+    [breite,laenge],
+    13
+);
+
+// Openstreetmap einbauen
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+
+// Positionsmarker hinzufügen
+let pin = L.marker(
+    [breite,laenge]
+).addTo(karte);
+
+// Popup zum pin hängen
+pin.bindPopup(titel).openPopup();
