e805ff0e384bf0e4fe5c5ffe72ef7fe944f5cc0b
Honka <csaw2326@uibk.ac.at>
2019-05-22 18:45:48 +0200

    Steigung etappe o4

--- adler/main.js
+++ adler/main.js
@@ -166,9 +166,43 @@ new L.GPX("AdlerwegEtappeO4.gpx", {
 }).on('addline', function (e) {
     console.log('linie geladen');
     const controlElevation = L.control.elevation({
+        //position: "bottomright",
+        //collapsed: true,
+        //detachedView: false,
         detachedView: true,
         elevationDiv: "#elevation-div",
     });
     controlElevation.addTo(karte);
     controlElevation.addData(e.line);
-}).addTo(karte);
+    const gpxLinie = e.line.getLatLngs();
+    console.log(gpxLinie);
+    for (let i = 1; i < gpxLinie.length; i +=1) {
+        //console.log(gpxLinie[i]);
+        let p1 = gpxLinie[i-1];
+        let p2 = gpxLinie[i];
+        let dist = karte.distance(
+            [ p1.lat,p1.lng ],
+            [ p2.lat,p2.lng ]
+        );
+            let delta = (p2.meta.ele - p1.meta.ele);
+            let proz = (dist != 0 ? delta / dist * 100.0 : 0).toFixed(1);
+            console.log('Distanz:', dist, 'Höhendiff:', delta, 'Steigung:', proz);
+            let farbe =
+                proz >= 10 ? "#d73027" :
+                proz >= 6 ? "#fc8d59" :
+                proz >= 2 ? "#fee08b" : 
+                proz >= 0 ? "#ffffbf" :
+                proz >= -6 ? "#d9ef8b" :
+                proz >= -10 ? "#d9ef8b" :
+                                "#1a9850";
+            //['#d73027','#fc8d59','#fee08b','#ffffbf','#d9ef8b','c','#1a9850']
+            L.polyline(
+            [
+                [ p1.lat,p1.lng ],
+                [ p2.lat,p2.lng ],
+            ], {
+                color : farbe,
+            }
+        ).addTo(karte);
+    }
+});
